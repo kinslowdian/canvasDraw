c7ac41a4b6285596e60baf152bf9aa583f4c33b1
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-08-22 14:33:39 +0100

    First main build

---
+++ public/js/_.js
@@ -0,0 +1,81 @@
+// DEBUG
+var trace = function(msg){ console.log(msg); };
+
+var canvas;
+var context;
+
+var displayProps;
+
+var drawShowBasic = false;
+
+var freq;
+
+function pageLoad_init()
+{
+	trace("pageLoad_init();");
+
+	displayProps = {};
+	displayProps.w = window.screen.width;
+	displayProps.h = window.screen.height;
+
+	draw_init();
+}
+
+function draw_init()
+{
+	canvas = document.querySelector(".display");
+	context = canvas.getContext("2d");
+
+	context.lineWidth = 0.01;
+	context.strokeStyle = '#ff0033';
+
+	canvas.width = displayProps.w;
+	canvas.height = displayProps.h;
+
+	if(drawShowBasic)
+	{
+		draw_basic();
+	}
+
+	else
+	{
+		context.beginPath();
+		draw_advanced();
+	}
+}
+
+function draw_basic()
+{
+	context.beginPath();
+
+	for(var i = 0; i < 6000; i++)
+	{
+		var x;
+		var y;
+
+		x = Math.random() * displayProps.w;
+		y = Math.random() * displayProps.h;
+
+		context.lineTo(x, y);
+	}
+
+	context.stroke();	
+}
+
+function draw_advanced()
+{
+	var x;
+	var y;
+	
+	// context.beginPath();
+
+	x = Math.random() * displayProps.w;
+	y = Math.random() * displayProps.h;
+
+	context.lineTo(x, y);
+
+	context.stroke();
+
+	freq = setTimeout(draw_advanced, 20);
+}
+
