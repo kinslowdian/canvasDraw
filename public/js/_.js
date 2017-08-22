235f657ea2adc8656fb93e0b046b0602b271dd7a
Kinslow <simon.kinslow@aa7778.weu.grey.global>
2017-08-22 15:10:47 +0100

    Double canvas test
    Faulty build, only works with first reference

--- public/js/_.js
+++ public/js/_.js
@@ -1,12 +1,12 @@
 // DEBUG
 var trace = function(msg){ console.log(msg); };
 
-var canvas;
-var context;
+var canvas0;
+var context0;
+var canvas1;
+var context1;
 
 var displayProps;
-
-var freq;
 
 function pageLoad_init()
 {
@@ -21,31 +21,42 @@ function pageLoad_init()
 
 function draw_init()
 {
-	canvas = document.querySelector(".display");
-	context = canvas.getContext("2d");
+	canvas0 = document.querySelector(".display0");
+	context0 = canvas0.getContext("2d");
 
-	canvas.width = displayProps.w;
-	canvas.height = displayProps.h;
+	canvas0.width = displayProps.w;
+	canvas0.height = displayProps.h;
 
-	context.beginPath();
-	draw_advanced();
+	context0.beginPath();
+
+	canvas1 = document.querySelector(".display1");
+	context1 = canvas1.getContext("2d");
+
+	canvas1.width = displayProps.w;
+	canvas1.height = displayProps.h;
+
+	context1.beginPath();
+	
+	draw_advanced(context0, 'rgba(255, 0, 51, 0.4)', 20);
+	draw_advanced(context1, 'rgba(0, 0, 0, 0.4)', 40);
 }
 
-function draw_advanced()
+function draw_advanced(ctx, c, r)
 {
 	var x;
 	var y;
+	var freq;
 
 	x = Math.random() * displayProps.w;
 	y = Math.random() * displayProps.h;
 
-	context.lineWidth = 0.01;
-	context.strokeStyle = 'rgba(255, 0, 51, 0.4)';
+	ctx.lineWidth = 0.01;
+	ctx.strokeStyle = c;
 
-	context.lineTo(x, y);
+	ctx.lineTo(x, y);
 
-	context.stroke();
+	ctx.stroke();
 
-	freq = setTimeout(draw_advanced, 20);
+	freq = setTimeout(draw_advanced, r, ctx, c);
 }
 
